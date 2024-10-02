b494429ac7e95a01f087e917c737445da3dc8481
Vinothkumar S <156108218+Vinoth-vk05@users.noreply.github.com>
2024-10-02 21:54:59 +0530

    Update loading.py

--- loading.py
+++ loading.py
@@ -4,12 +4,8 @@ import pickle
 
 data = {}
 f1 = fr.load_image_file("faces/2022pecai278.jpg")
-f2 = fr.load_image_file("faces/2022pecai282.jpg")
-f3 = fr.load_image_file("faces/2022pecai263.jpg")
 
 data['2022pecai278'] = fr.face_encodings(f1)[0]
-data['2022pecai282'] = fr.face_encodings(f2)[0]
-data['2022pecai263'] = fr.face_encodings(f3)[0]
 
 with open("data/encodings_index.dat", 'wb') as f:
     pickle.dump(data, f)
